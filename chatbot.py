59da38169d2893496b1f124053757c4d19a8d775
VaibhavRamrakhyani <89211694+VaibhavRamrakhyani@users.noreply.github.com>
2024-07-22 03:42:31 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -72,7 +72,7 @@ db = load_pdf_documents('./corpus/')
 
 # Define prompt template
 DEFAULT_TEMPLATE = """
-### Instruction: You're a travelling support agent that is talking to a customer. Use only the chat history and the following information
+### Instruction: You're a support agent that is talking to a customer. Use only the chat history and the following information
 {context}
 to answer in a helpful manner to the question. If you don't know the answer - say that you don't know. Keep your replies short, compassionate and informative.
 {chat_history}
